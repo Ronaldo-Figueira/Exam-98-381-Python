19cdec0b1ca472d9cb550a5af0b9931ac2adb0d8
Herick Freitas <herickfreitas@gmail.com>
2018-04-08 22:08:34 -0300

    Modulo 12 e 13
    Concluído

---
+++ 002 - Estudo direcionado/Projeto/Aula12 - Leitura de arquivos.py
@@ -0,0 +1,42 @@
+############## READ
+
+# Efetuando a leitura de um arquivo
+#clientes = open("clientes.txt", "r", encoding="utf8")
+#clientesModelo = clientes.read()
+#print(clientesModelo)
+
+# Efetuando leitura de linha de CSV tabular
+#clientes = open("clientes.txt", "r", encoding="utf8")
+#clientesModeloLinha = clientes.readline()
+#print(clientesModeloLinha)
+
+
+# Efetuando leitura de CSV usando a biblioteca import CSV
+#import csv
+
+#with open("CLIENTES.CSV", "r", encoding="utf8") as myCSVFile:
+#    clientes = csv.reader(myCSVFile)
+
+#    for linha in clientes:
+#        print(linha)
+
+# Efetuando leitura do item da linha 
+#import csv
+
+#with open("CLIENTES.CSV", "r", encoding="utf8") as myCSVFile:
+#    clientes = csv.reader(myCSVFile, delimiter=";")
+
+#    for linha in clientes:
+#        print(linha)
+#        for item in linha:
+#            print(item)
+
+# Alterando separador do arquivo para vizualização
+#import csv
+
+#with open("CLIENTES.CSV", "r", encoding="utf8") as myCSVFile:
+#    clientes = csv.reader(myCSVFile, delimiter=";")
+
+#    for linha in clientes:
+#        print('|'.join(linha))
+
---
+++ 002 - Estudo direcionado/Projeto/Aula13 - Funções.py
@@ -0,0 +1,35 @@
+## Sintaxe básica de uma Função
+#def printMenssagem():
+#    print("Hello world!")
+#    return
+
+## Sintaxe de execução da função
+#printMenssagem()
+
+
+## Sintaxe de uma Função utilizando função main()
+#def main():
+#    printMenssagem()
+#    return
+
+#def printMenssagem():
+#    print("Hello world!")
+#    return
+    
+## chamada da função
+#main()
+
+## Definindo função com variável (texto)
+#def printMessangem(texto):
+#    print(texto)
+#    return
+
+## Efetuando chamada da função e informando o texto para o parêmtro
+#printMessangem("Hello World!")
+
+def myPrintout(message) :
+    message = " " + message
+    return message
+print("Olá")
+print(myPrintout("Mundo"))
+
